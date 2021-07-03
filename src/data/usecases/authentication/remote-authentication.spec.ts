83e3f5bbbcae29f8169a73d26fc12bf9edc9782b
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-03 18:42:34 -0300

    test: add body on auth protocol

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,6 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 import faker from 'faker'
 type SutTypes = {
   sut: RemoteAuthentication
@@ -14,7 +15,14 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL ', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body ', async () => {
+    const authenticationParams = mockAuthentication()
+    const { sut, httpPostClientSpy } = makeSut()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
