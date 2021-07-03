2494a6c5ee61206f68f6eaabe6d986572ad01850
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-03 20:16:20 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,9 +3,10 @@ import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AccountModel } from '@/domain/model/account-model'
 
 export class RemoteAuthentication {
-  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) { }
+  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) { }
 
   async auth (params: AuthenticationParams): Promise<void> {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
