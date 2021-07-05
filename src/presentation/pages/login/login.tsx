6c0f7f6259f87c9d75242a46599e7f255a7059cc
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-04 21:12:24 -0300

    feat: ensure that Login is not rendering spinner and erroMessage on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
-
+import Context from '@/presentation/contexts/form/form-context'
+type StateProps= {
+  isLoading: boolean
+  errorMessage: string
+}
 const Login: React.FC = () => {
+  const state = useState<StateProps>({ isLoading: false, errorMessage: '' })
   return (
     <div className={Styles.login}>
       <LoginHeader/>
+      <Context.Provider value={state}>
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input type='email' name='email' placeholder={'Digite seu e-mail'}/>
@@ -14,6 +20,7 @@ const Login: React.FC = () => {
         <span className={Styles.link}>Criar Conta</span>
         <FormStatus/>
       </form>
+      </Context.Provider>
       <Footer/>
     </div>
   )
