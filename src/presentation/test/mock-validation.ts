4fec3c0005b20c9bc314957d7e8bf5b70a5fff34
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-08 00:56:53 -0300

    test: ensure Login shows error when email and password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -2,12 +2,12 @@ import { Validation } from '@/presentation/protocols/validation'
 
 export class ValidationSpy implements Validation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
+  fieldName: string[] = []
+  fieldValue: string[] = []
 
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
+    this.fieldName.push(fieldName)
+    this.fieldValue.push(fieldValue)
     return this.errorMessage
   }
 }
