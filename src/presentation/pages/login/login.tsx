dd39d10d9437b9d18c96739d54e3f8520ed53ae9
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-08 02:15:59 -0300

    feat: ensure Authentication is called with correct values when form is submitted

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,11 +3,14 @@ import Styles from './login-styles.scss'
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
+import { Authentication } from '@/domain/usecases'
+import { stat } from 'fs'
 
 type Props ={
   validation: Validation
+  authentication: Authentication
 }
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     errorMessage: '',
@@ -26,13 +29,14 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   }
   , [state.email, state.password])
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     setState({
       ...state,
       ...state,
       isLoading: true
     })
+    await authentication.auth({email: state.email, password: state.password})
   }
 
   return (
