89626c1beb979037febca428fe7c7762bbb891d9
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-26 00:05:36 -0300

    test: ensure ValidationBuilder returns the expected validations to email

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -5,4 +5,8 @@ describe('Validation Builder', function () {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toContainEqual(new RequiredFieldValidation('any_field'))
   })
+  test('Should Validation Builder return RequiredFieldValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toContainEqual(new RequiredFieldValidation('any_field'))
+  })
 })
