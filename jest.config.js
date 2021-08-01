a87b1be7c51dfd736cc31e99435dbf658467ad6e
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-31 21:06:35 -0300

    chore: update jest to ignore index files

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/components/router/**/*',
-    '!**/*.d.ts'
+    '!**/*.d.ts',
+    '!**/index.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
