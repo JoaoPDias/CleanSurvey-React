1e24d2b3009b075cc383b1132b777a36c816c583
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-07-31 23:12:32 -0300

    refactor: create MakeSut method for LocalSaveAccessToken Test

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -2,10 +2,25 @@ import * as faker from 'faker'
 import { SetStorageSpy } from '@/data/test'
 import { LocalSaveAccessToken } from '@/data/usecases/save-access-token/local-save-access-token'
 
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+  return {
+    sut,
+    setStorageSpy
+  }
+}
 describe('LocalSaveAccessToken', function () {
   test('Should LocalSaveAccessToken', async () => {
-    const setStorageSpy = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorageSpy)
+    const {
+      sut,
+      setStorageSpy
+    } = makeSut()
     const accessToken = faker.datatype.uuid()
     await sut.save(accessToken)
     expect(setStorageSpy.key).toBe('accessToken')
