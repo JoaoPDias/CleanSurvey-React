e5da19dd12171225a0fa62bbec4ff80c9c4e08b4
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-08-01 00:41:13 -0300

    feat: create factories to use SaveAccessToken abstraction

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeLocalSaveToken } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (<Login
     authentication={makeRemoteAuthentication()}
-    validation={makeLoginValidation()}/>)
+    validation={makeLoginValidation()}
+    saveAccessToken={makeLocalSaveToken()}
+  />)
 }
