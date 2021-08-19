62300b081fd7355d21d6f27c1e9ecd5255d3b082
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-08-18 23:51:46 -0300

    test: should Login load with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,15 @@
 describe('Login', function () {
-  it('Should Login load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('should Login load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('errorWrap').should('not.have.descendants')
   })
 })
