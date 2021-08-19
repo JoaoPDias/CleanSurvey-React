aa343b34e8c9de07e2c8064985275f331e0ff4b1
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-08-18 23:59:23 -0300

    test: should Login Page show error when form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,13 +1,29 @@
+import * as faker from 'faker'
+
 describe('Login', function () {
   beforeEach(() => {
     cy.visit('login')
   })
-  it('should Login load with correct initial state', () => {
+  it('should Login Page load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly')
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
       .should('contain.text', '🔴')
+    cy.getByTestId('password').should('have.attr', 'readonly')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('errorWrap').should('not.have.descendants')
+  })
+  it('should Login Page show error when form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.words(3))
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor Inválido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor Inválido')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('errorWrap').should('not.have.descendants')
