39c36ee7865989a60bb89e9b461c740ecd8602bc
Joao Paulo Dias <jpdias1997@hotmail.com>
2021-08-22 00:46:45 -0300

    feat: should LocalSaveAccessToken throw an error when accessToken is falsy

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,5 +1,6 @@
 import { SaveAccessToken } from '@/domain/usecases'
 import { SetStorage } from '@/data/protocols/cache'
+import { UnexpectedError } from '@/domain/errors'
 
 export class LocalSaveAccessToken implements SaveAccessToken {
   constructor (private readonly setStorage: SetStorage) {
@@ -7,6 +8,9 @@ export class LocalSaveAccessToken implements SaveAccessToken {
   }
 
   async save (accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     await this.setStorage.set('accessToken', accessToken)
   }
 }
